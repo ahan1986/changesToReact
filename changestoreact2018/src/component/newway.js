09e23d8318aeb6ce6a75800966963d278582a970
Handrew <andrew.a.han@gmail.com>
2018-10-29 17:34:28 -0400

    add surname and setSurname and methods to call setSurname

--- changestoreact2018/src/component/newway.js
+++ changestoreact2018/src/component/newway.js
@@ -3,10 +3,18 @@ import React, { useState } from 'react'
 // useState is a hook provided by React.  Hook is a function that lets you look into react features from your function compontents
 
 export default function Oldway(props) {
-    const [name, setName] = useState("Andrew");
+    // setName ( ??? ) === setState({ name: ??? })
+    //when we call setName or setSurname, it tells react to re-render this component just like if we called setState. so next time react renders the component, it is going to pass the current name and surname to the component and we can use access it quickly instead of using setState.
+    const [name, setName] = useState("Andrew"); // inside the parameters for useState is the initial value
+    const [surname, setSurname] = useState("Han");
 
     function handleNameChange(e) {
+
         setName(e.target.value);
+    }
+
+    function handlesurnameChange(e) {
+        setSurname(e.target.value);
     }
 
     return (
@@ -15,6 +23,13 @@ export default function Oldway(props) {
                 <input
                     value={name}
                     onChnage={handleNameChange}
+                />
+            </div>
+
+            <div>
+                <input
+                    value={surname}
+                    onChnage={handlesurnameChange}
                 />
             </div>
         </section>
